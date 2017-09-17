73a60f97c791330d0f774d95a8fbc62aa785aa0b
Art Yabkevich <yabkevich.artsiom.09@gmail.com>
2017-09-17 15:37:58 -0400

    alert with prompt added

--- src/pages/quotes/quotes.ts
+++ src/pages/quotes/quotes.ts
@@ -1,5 +1,5 @@
 import { Quote } from '../../data/quote.interface';
-import { NavParams } from 'ionic-angular';
+import { AlertController, NavParams } from 'ionic-angular';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -9,7 +9,10 @@ import { Component, OnInit } from '@angular/core';
 export class QuotesPage implements OnInit {
   quoteGroup: { category: string, quotes: Quote[], icon: string };
 
-  constructor (private navParams: NavParams) {}
+  constructor (
+    private navParams: NavParams,
+    private alertCtrl: AlertController
+  ) {}
 
   ngOnInit() {
     this.quoteGroup = this.navParams.data;
@@ -21,4 +24,29 @@ export class QuotesPage implements OnInit {
   //  Ex: {{ quoteGroup?.category}}
   //  Page template renders before data becomes available so it gives an error
   // }
+
+  onAddToFavourite(selectedQuote: Quote) {
+    const alert = this.alertCtrl.create({
+      title: 'Add Quote',
+      subTitle: 'Are you sure?',
+      message: 'Are you sure you want to add the quote?',
+      buttons: [
+        {
+          text: 'Yes, go ahead',
+          handler: () => {
+            console.log('Ok');
+          }
+        },
+        {
+          text: 'No, I changed my mind!',
+          role: 'cancel',
+          handler: () => {
+            console.log('Canceled!');
+          }
+        }
+      ]
+    });
+
+    alert.present();
+  }
 }
