314291ca05850a48665f760d060ae04e98708114
Art Yabkevich <yabkevich.artsiom.09@gmail.com>
2017-09-17 16:06:51 -0400

    modal windows injected

--- src/pages/favorites/favorites.ts
+++ src/pages/favorites/favorites.ts
@@ -1,3 +1,5 @@
+import { QuotePage } from '../quote/quote';
+import { ModalController } from 'ionic-angular';
 import { QuotesService } from '../../services/quotes';
 import { Component } from '@angular/core';
 
@@ -11,11 +13,17 @@ export class FavoritesPage {
   quotes: Quote[];
 
   constructor (
-    private quotesService: QuotesService
+    private quotesService: QuotesService,
+    private modalCtrl: ModalController
   ) {}
 
   ionViewWillEnter(){
     this.quotes = this.quotesService.getFavoriteQuotes();
   }
-  
+
+  onViewQuote(quote: Quote) {
+    const modal = this.modalCtrl.create(QuotePage);
+    modal.present();
+  }
+
 }
