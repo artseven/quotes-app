c7632f6573a80b53d03af6c1a276da6434b93891
Art Yabkevich <yabkevich.artsiom.09@gmail.com>
2017-09-17 13:37:13 -0400

    js object added as data

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'page-tabs',
     template: `
-        <ion-tabs>
+        <ion-tabs selectedIndex="0">
             <ion-tab [root]="favoritesPage" tabTitle="Favorites" tabIcon="star"></ion-tab>
             <ion-tab [root]="libraryPage" tabTitle="Library" tabIcon="book"></ion-tab>
         </ion-tabs>
