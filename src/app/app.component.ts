de25626dd10809e8064b062d474613e9e067f092
Art Yabkevich <yabkevich.artsiom.09@gmail.com>
2017-09-18 14:53:07 -0400

    changing rootPage with ViewChild and menuController

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,9 @@
+
 import { SettingsPage } from '../pages/settings/settings';
 import { TabsPage } from '../pages/tabs/tabs';
-import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+
+import { NavController, MenuController, Platform } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
@@ -12,18 +14,21 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 export class MyApp {
   tabsPage = TabsPage;
   settingsPage = SettingsPage;
+  @ViewChild('nav') nav: NavController
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
-    platform.ready().then(() => {
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
-      statusBar.styleDefault();
-      splashScreen.hide();
-    });
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen,
+    private menuCtrl: MenuController) {
+      platform.ready().then(() => {
+        // Okay, so the platform is ready and our plugins are available.
+        // Here you can do any higher level native things you might need.
+        statusBar.styleDefault();
+        splashScreen.hide();
+      });
   }
 
   onLoad(page: any) {
-    this.
+    this.nav.setRoot(page);
+    this.menuCtrl.close();
   }
 }
 
